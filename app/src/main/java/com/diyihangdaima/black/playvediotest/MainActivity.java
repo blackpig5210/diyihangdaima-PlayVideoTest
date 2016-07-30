cf2faff52b1a2a66e8d6044b42a1dd688d49a37c
blackpig <blackpig5210@gmail.com>
2016-07-31 07:50:37 +0800

    first commit

---
+++ app/src/main/java/com/diyihangdaima/black/playvediotest/MainActivity.java
@@ -0,0 +1,67 @@
+package com.diyihangdaima.black.playvediotest;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private VideoView videoView;
+    private Button play;
+    private Button pause;
+    private Button replay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        replay = (Button) findViewById(R.id.replay);
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        initVideoPath();
+    }
+
+    private void initVideoPath() {
+        File file = new File(Environment.getExternalStorageDirectory(), "video.mp4");
+        videoView.setVideoPath(file.getPath());//指定视频文件路径
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.play:
+                if (!videoView.isPlaying()) {
+                    videoView.start();//开始播放
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause();//暂停播放
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()) {
+                    videoView.resume();//重新播放
+                }
+
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();//该方法将VideoView所占用的资源释放掉
+        }
+    }
+}
